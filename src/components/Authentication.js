ebfd60d3bf1b27a69d04d66777441a81f0885b49
MichalIrzylowski <michal.irzylowski@gmail.com>
2018-09-02 21:11:07 +0200

    ADD Validation to auth form

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,72 +1,140 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import { REGISTER_REQUEST, LOGIN_REQUEST } from '../redux/ActionTypes';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import validator from "validator";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { REGISTER_REQUEST, LOGIN_REQUEST } from "../redux/ActionTypes";
+import ValidateFormAlert from "./ValidateFormAlert";
 
 class Authentication extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: ''
+      email: "",
+      password: "",
+      errors: { email: "", password: "" }
+    };
+  }
+
+  handleChange = name => e => {
+    this.setState({
+      ...this.state,
+      [name]: e.target.value
+    });
+  };
+
+  componentDidUpdate(prevState) {
+    if (this.props.user.isAuthenticated !== prevState.user.isAuthenticated) {
+      this.props.history.push("/");
     }
   }
 
- handleChange = name => e => {
-   this.setState({
-     ...this.state,
-     [name]: e.target.value
-   })
- }
+  validate = (email, password) => {
+    let errors = {};
+    if (!validator.isEmail(email)) {
+      errors.email = "Please put valid email!";
+    }
+    if (!validator.isLength(password, { min: 8, max: undefined })) {
+      errors.password = "Password has to have at least 8 characters!";
+    }
+    return errors;
+  };
 
- componentDidUpdate(prevState) {
-   if (this.props.user.isAuthenticated !== prevState.user.isAuthenticated) {
-     this.props.history.push('/');
-   }
- }
+  handleRegister = e => {
+    e.preventDefault();
+    const formErrors = this.validate(this.state.email, this.state.password);
+    this.setState({ ...this.state, errors: formErrors });
+    if (Object.keys(formErrors).length === 0) {
+      this.props.sendRegisterData({
+        email: this.state.email,
+        password: this.state.password
+      });
+      this.setState({ email: "", password: "", errors: {} });
+    }
+  };
 
- handleRegister = e => {
-   e.preventDefault();
-   this.props.sendRegisterData(this.state);
-   this.setState({email:'', password: ''});
- }
-
- handleLogin = e => {
-   e.preventDefault();
-   this.props.sendLoginData(this.state);
-   this.setState({email:'', password: ''});
- }
+  handleLogin = e => {
+    e.preventDefault();
+    const formErrors = this.validate(this.state.email, this.state.password);
+    this.setState({ ...this.state, errors: formErrors });
+    if (Object.keys(formErrors).length === 0) {
+      this.props.sendLoginData({
+        email: this.state.email,
+        password: this.state.password
+      });
+      this.setState({ email: "", password: "", errors: {} });
+    }
+  };
 
   render() {
     return (
-      <Form onSubmit={this.props.match.path === '/Register' ? this.handleRegister : this.handleLogin}>
-        {this.props.match.path === '/Register' ? <h2>Register yourself</h2> : <h2>Login yourself!</h2> }
-        {this.props.match.path === '/Register' ? <p>Remember to put fake data!</p> : <p>Put your login data below.</p>}
+      <Form
+        onSubmit={
+          this.props.match.path === "/Register"
+            ? this.handleRegister
+            : this.handleLogin
+        }
+      >
+        {this.props.match.path === "/Register" ? (
+          <h2>Register yourself</h2>
+        ) : (
+          <h2>Login yourself!</h2>
+        )}
+        {this.props.match.path === "/Register" ? (
+          <p>Remember to put fake data!</p>
+        ) : (
+          <p>Put your login data below.</p>
+        )}
         <FormGroup>
-          <Label htmlFor='email'>Email</Label>
-          <Input name='email' id='email' type='email' placeholder='example@example.com' value={this.state.email} onChange={this.handleChange('email')} />
+          <Label htmlFor="email">Email</Label>
+          <Input
+            name="email"
+            id="email"
+            type="text"
+            placeholder="example@example.com"
+            value={this.state.email}
+            onChange={this.handleChange("email")}
+          />
+          {this.state.errors.email && (
+            <ValidateFormAlert text={this.state.errors.email} />
+          )}
         </FormGroup>
         <FormGroup>
-          <Label htmlFor='password'>Password</Label>
-          <Input name='password' id='password' type='password' placeholder='Make it secure!' value={this.state.password} onChange={this.handleChange('password')} />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            placeholder="Make it secure!"
+            value={this.state.password}
+            onChange={this.handleChange("password")}
+          />
+          {this.state.errors.password && (
+            <ValidateFormAlert text={this.state.errors.password} />
+          )}
         </FormGroup>
-        <Button color='success'>{this.props.match.path === '/Register' ? 'Register!' : 'Sign up!'}</Button>
+        <Button color="success">
+          {this.props.match.path === "/Register" ? "Register!" : "Sign up!"}
+        </Button>
       </Form>
-    )
+    );
   }
 }
 
-function mapDispatchToProps (dispatch) {
-  return  {
-    sendRegisterData: (data) => dispatch({type: REGISTER_REQUEST, payload: data}),
-    sendLoginData: (data) => dispatch({type: LOGIN_REQUEST, payload: data})
-  }
+function mapDispatchToProps(dispatch) {
+  return {
+    sendRegisterData: data =>
+      dispatch({ type: REGISTER_REQUEST, payload: data }),
+    sendLoginData: data => dispatch({ type: LOGIN_REQUEST, payload: data })
+  };
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     user: state.currentUser
-  }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Authentication);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Authentication);
---
+++ src/components/ValidateFormAlert.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ValidateFormAlert = ({ text }) => {
+  return <p className="ValidateFormAlert">{text}</p>;
+};
+
+export default ValidateFormAlert;
